363b622f4adb66b5b36d823eeae8e9a2f17f5692
Ondrej Platek <ondrej.platek@seznam.cz>
2012-12-07 15:37:45 +0100

    Set up latex listings for C and Pytho

---
+++ thesis-text/snippets/test.c
@@ -0,0 +1,9 @@
+// Author:   Ondrej Platek, Copyright 2012, code is without any warranty!
+// Created:  13:55:18 07/12/2012
+// Modified: 13:55:18 07/12/2012
+
+#include <stdio.h>
+
+int main(int argc, char ** argv){
+    printf('%d', argc)
+}
---
+++ thesis-text/snippets/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# Author:   Ondrej Platek, Copyright 2012, code is without any warranty!
+# Created:  13:50:12 07/12/2012
+# Modified: 13:50:12 07/12/2012
+
+import numpy
+
+
+class Test:
+    print 'ahoj'
+
+    def __init__(self):
+        self.t = 'aha'
+        print(numpy.arange(10))
