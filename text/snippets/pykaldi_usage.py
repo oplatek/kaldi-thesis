af8ef7862a675cbb66509e4544c3019953484a89
Ondrej Platek <ondrej.platek@seznam.cz>
2014-02-24 16:14:20 +0100

    added few lines and python API decoder example

---
+++ text/snippets/pykaldi_usage.py
@@ -0,0 +1,10 @@
+d = PyGmmLatgenWrapper()
+d.setup(argv)
+while audio_to_process():
+    d.frame_in(get_raw_pcm_audio())
+    dec_t = d.decode(max_frames=10)
+    while dec_t > 0:
+        decoded_frames += dec_t
+        dec_t = d.decode(max_frames=10)
+d.prune_final()
+lik, lat = d.get_lattice()
